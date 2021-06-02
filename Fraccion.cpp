1ea0b1335ff30a257d570f0acbb056ea43a095c0
A01633793 <74060872+A01633793@users.noreply.github.com>
2021-06-02 17:55:54 -0500

    Update Fraccion.cpp

--- Fraccion.cpp
+++ Fraccion.cpp
@@ -2,83 +2,82 @@
 #include <iostream>
 using namespace std;
 
+//constructor por default
 Fraccion::Fraccion(){
-    numerador = 0;
-    denominador = 1;
+    num= 1;
+    denom= 1;
 }
 
-Fraccion::Fraccion(int num, int den){
+Fraccion::Fraccion(int numm, int den){
     if(den == 0)
         den = 1;
     // ambos negativos o unicamente denominador negativo, entonces cambio signo en ambos
     // La expresion completa sería den < 0 && num < 0 || num > 0 && den < 0
     // Sin embargo, tienen en comun que si el denominador es negativo, cambio el signo en ambos 
     if(den < 0 ){ 
-        num = -num;
+        numm = -numm;
         den = -den;
     }
-    reduccion(num,den);
-    numerador = num;
-    denominador = den;
+    reduccion(numm,den);
+    this->num = numm;
+    this->denom = den;
 }
 
-void Fraccion::reduccion(int &num, int &den){
+void Fraccion::reduccion(int &numm, int &den){
     // Variables auxiliares
     int limit = 0;
     int signo1 = 1;
     int signo2 = 1;
     // Guarda el signo en las variables signoX y deja num y den sin signo.
     // Esto se hace para que las operaciones modulo y limite esten correctas logicamente
-    if(num < 0){
+    if(numm < 0){
         signo1 = -1;
-        num = num*signo1;
+        numm = numm*signo1;
     }
     if(den < 0){
         signo2 = -1;
         den = den*signo2;
     }
-    // La variable limite es la menor entre el denominador y el numerador
-    // Sirve como criterio de parada al identificar un posible maximo comun divisor.
-    if(num >= den)
+    //aquí se identifica si hay un posible MCD (maximo comun divisor)
+    if(numm >= den)
         limit = den; 
     else
-        limit = num;
+        limit = numm;
     int it = 2;
     while(it<=limit){
-        if(num % it == 0 && den % it == 0){
-            num = num/it;
+        if(numm % it == 0 && den % it == 0){
+            numm = numm/it;
             den = den/it;
         }else{
             it++;
         }
     }
     // Devolvemos los signos del numerador y denominador (en caso de que exista cambio)
-    num = num*signo1;
+    numm = numm*signo1;
     den = den*signo2;
 }
 
 Fraccion Fraccion::suma(Fraccion farg){
-    int num = numerador * farg.getDenominador() +  denominador * farg.getNumerador();
-    int den = denominador * farg.getDenominador();
-    Fraccion fresultado(num,den);
+    int numm = num * farg.getDenominador() +  denom * farg.getNumerador();
+    int den = denom * farg.getDenominador();
+    Fraccion fresultado(numm,den);
     return fresultado;
 }
 
 Fraccion Fraccion::multiplicacion(Fraccion farg){
-    int num = numerador * farg.getNumerador();
-    int den = denominador * farg.getDenominador();
-    Fraccion fresultado(num,den);
+    int numm = num * farg.getNumerador();
+    int den = denomr * farg.getDenominador();
+    Fraccion fresultado(numm,den);
     return fresultado;  
 }
 
 void Fraccion::imprimir(){
-    cout << numerador << "/" << denominador << endl;
+    cout << num << "/" << denom << endl;
 }
 
-int Fraccion::getNumerador(){return numerador;}
-int Fraccion::getDenominador(){return denominador;}
-void Fraccion::setNumerador(int num){
-    this->numerador = num;
-    //cout << "nuevo numerador" << this->numerador << endl;
+int Fraccion::getNumerador(){return num;}
+int Fraccion::getDenominador(){return denom;}
+void Fraccion::setNumerador(int numm){
+    this->num = numm;
     }
-void Fraccion::setDenominador(int den){denominador = den;}
+void Fraccion::setDenominador(int den){denom = den;}
